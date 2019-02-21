1714f7b6c49019d1d2cd651dbf892b8f02e70b32
Paul Davis <p.davis@criteo.com>
2019-02-21 11:56:34 -0800

    Tweak Build Scripts
    - Move configurations, ARCHS, and VALID_ARCHS to variables
    - Move sets to top of the scripts
    - Rename CONFIGURATION to CRITEO_CONFIGURATION
    
    Change-Id: Ibf149e96a37bd5f6eef88efaea2e91d4889679d9
    JIRA: EE-131
    
    Former-commit-id: e58dfb0319908a6424c01317e6a98e97b233c6b6

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,45 +1,48 @@
 #!/bin/bash -l
 
 set +x
+set -Eeuo pipefail
 
 rm -rf build/output
 mkdir -p build/output/sim
 
-CONFIGURATION="Release"
+CRITEO_ARCHS='armv7 armv7s arm64'
+CRITEO_SIM_ARCHS='i386 x86_64'
 
-set -Eeuo pipefail 
+CRITEO_CONFIGURATION="Release"
+
     xcodebuild \
     -workspace fuji.xcworkspace \
         -scheme pubsdk \
-        -configuration $CONFIGURATION \
+        -configuration $CRITEO_CONFIGURATION \
         -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=`sysctl -n hw.ncpu` \
         -derivedDataPath build/DerivedData  \
         -sdk iphonesimulator \
         -destination 'platform=iOS Simulator,name=iPhone XS,OS=latest' \
-        ARCHS='i386 x86_64' \
-        VALID_ARCHS='i386 x86_64' \
+        ARCHS="$CRITEO_SIM_ARCHS" \
+        VALID_ARCHS="$CRITEO_SIM_ARCHS" \
         ONLY_ACTIVE_ARCH=NO \
         clean build | xcpretty
 
-        cp -R "build/DerivedData/Build/Products/$CONFIGURATION-iphonesimulator/pubsdk.framework" build/output/sim
+        cp -R "build/DerivedData/Build/Products/$CRITEO_CONFIGURATION-iphonesimulator/pubsdk.framework" build/output/sim
 
         mkdir -p build/output/device
 
     xcodebuild \
     -workspace fuji.xcworkspace \
         -scheme pubsdk \
-        -configuration $CONFIGURATION \
+        -configuration $CRITEO_CONFIGURATION \
         -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=`sysctl -n hw.ncpu` \
         -derivedDataPath build/DerivedData  \
         -sdk iphoneos \
-        ARCHS='armv7 armv7s arm64 arm64e' \
-        VALID_ARCHS='armv7 armv7s arm64 arm64e' \
+        ARCHS="$CRITEO_ARCHS" \
+        VALID_ARCHS="$CRITEO_ARCHS" \
         ONLY_ACTIVE_ARCH=NO \
         CODE_SIGN_IDENTITY="" \
         CODE_SIGNING_REQUIRED=NO \
         build | xcpretty
 
-        cp -R "build/DerivedData/Build/Products/$CONFIGURATION-iphoneos/pubsdk.framework" build/output/device
+        cp -R "build/DerivedData/Build/Products/$CRITEO_CONFIGURATION-iphoneos/pubsdk.framework" build/output/device
 
 cp -R build/output/device/pubsdk.framework build/output
 rm build/output/pubsdk.framework/pubsdk
@@ -48,45 +51,45 @@ lipo -create -output build/output/pubsdk.framework/pubsdk build/output/sim/pubsd
 
 cd build/output
 
-zip -r "pubsdk.framework.$CONFIGURATION.zip" pubsdk.framework
+zip -r "pubsdk.framework.$CRITEO_CONFIGURATION.zip" pubsdk.framework
 
 cd ../..
 
 
-CONFIGURATION="Debug"
+CRITEO_CONFIGURATION="Debug"
 
     xcodebuild \
     -workspace fuji.xcworkspace \
         -scheme pubsdk \
-        -configuration $CONFIGURATION \
+        -configuration $CRITEO_CONFIGURATION \
         -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=`sysctl -n hw.ncpu` \
         -derivedDataPath build/DerivedData  \
         -sdk iphonesimulator \
         -destination 'platform=iOS Simulator,name=iPhone XS,OS=latest' \
-        ARCHS='i386 x86_64' \
-        VALID_ARCHS='i386 x86_64' \
+        ARCHS="$CRITEO_SIM_ARCHS" \
+        VALID_ARCHS="$CRITEO_SIM_ARCHS" \
         ONLY_ACTIVE_ARCH=NO \
         clean build test | xcpretty --report junit --report html
 
-        cp -R "build/DerivedData/Build/Products/$CONFIGURATION-iphonesimulator/pubsdk.framework" build/output/sim
+        cp -R "build/DerivedData/Build/Products/$CRITEO_CONFIGURATION-iphonesimulator/pubsdk.framework" build/output/sim
 
         mkdir -p build/output/device
 
     xcodebuild \
     -workspace fuji.xcworkspace \
         -scheme pubsdk \
-        -configuration $CONFIGURATION \
+        -configuration $CRITEO_CONFIGURATION \
         -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=`sysctl -n hw.ncpu` \
         -derivedDataPath build/DerivedData  \
         -sdk iphoneos \
-        ARCHS='armv7 armv7s arm64 arm64e' \
-        VALID_ARCHS='armv7 armv7s arm64 arm64e' \
+        ARCHS="$CRITEO_ARCHS" \
+        VALID_ARCHS="$CRITEO_ARCHS" \
         ONLY_ACTIVE_ARCH=NO \
         CODE_SIGN_IDENTITY="" \
         CODE_SIGNING_REQUIRED=NO \
         build | xcpretty
 
-        cp -R "build/DerivedData/Build/Products/$CONFIGURATION-iphoneos/pubsdk.framework" build/output/device
+        cp -R "build/DerivedData/Build/Products/$CRITEO_CONFIGURATION-iphoneos/pubsdk.framework" build/output/device
 
 cp -R build/output/device/pubsdk.framework build/output
 rm build/output/pubsdk.framework/pubsdk
@@ -95,6 +98,6 @@ lipo -create -output build/output/pubsdk.framework/pubsdk build/output/sim/pubsd
 
 cd build/output
 
-zip -r "pubsdk.framework.$CONFIGURATION.zip" pubsdk.framework
+zip -r "pubsdk.framework.$CRITEO_CONFIGURATION.zip" pubsdk.framework
 
 cd ../..
