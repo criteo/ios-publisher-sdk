f4c54887da264a18450dda5d0d3e9432d03fd2c5
Sneha Pathrose <s.pathrose@criteo.com>
2019-06-20 17:22:11 -0700

    Changes to only include header bidding files Also change version to 2.1.0 in Info.plist and Current Project Version Change-Id: Iaed8e4d40b6f4da6b664af8c7024791625f2f996
    Former-commit-id: 7bdf58c71f38d2511db233a9f3d1e9f63c057e8d

--- pubsdk/CriteoPublisherSdk/CriteoPublisherSdk.h
+++ pubsdk/CriteoPublisherSdk/CriteoPublisherSdk.h
@@ -14,12 +14,7 @@ FOUNDATION_EXPORT double CriteoPublisherSdkVersionNumber;
 FOUNDATION_EXPORT const unsigned char CriteoPublisherSdkVersionString[];
 
 // Public Headers
-// TODO: Add CRBannerADUnit and CRInterstitialAdUnit
 #import <CriteoPublisherSdk/CRAdUnit.h>
 #import <CriteoPublisherSdk/CRBannerAdUnit.h>
 #import <CriteoPublisherSdk/CRInterstitialAdUnit.h>
 #import <CriteoPublisherSdk/Criteo.h>
-#import <CriteoPublisherSdk/CRBannerView.h>
-#import <CriteoPublisherSdk/CRInterstitial.h>
-#import <CriteoPublisherSdk/CRBannerViewDelegate.h>
-#import <CriteoPublisherSdk/CRInterstitialDelegate.h>
