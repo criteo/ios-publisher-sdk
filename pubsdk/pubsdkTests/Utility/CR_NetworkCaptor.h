48938c790355a58ed0cf515749492057bd78d86a
Romain Lofaso <r.lofaso@criteo.com>
2019-11-25 16:01:26 +0100

    Allow to analyse the network calls in the tests
    An alternative to the NetworkManagerDelegate. To be discussed.
    
    Change-Id: Idc4c33389390a1fa0a8acbe865646ed0c522673a
    JIRA: EE-593
    
    Former-commit-id: 06453420790894fa8203590725ef3259a16b401b

---
+++ pubsdk/pubsdkTests/Utility/CR_NetworkCaptor.h
@@ -0,0 +1,65 @@
+//
+//  CR_NetworkCaptor.h
+//  pubsdkTests
+//
+//  Created by Romain Lofaso on 11/25/19.
+//  Copyright © 2019 Criteo. All rights reserved.
+//
+
+#import "CR_NetworkManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class CR_HttpContent;
+
+typedef enum {
+    GET,
+    POST
+} CR_HTTPVerb;
+
+typedef void (^CR_HTTPRequestListener)(NSURL *, CR_HTTPVerb);
+
+/**
+ NetworkManager class that is used for a man-in-the-middle approach in the test.
+ Note: The name and the approach is similar in Android.
+ */
+@interface CR_NetworkCaptor : CR_NetworkManager
+
+@property (nonatomic, strong, readonly) CR_NetworkManager *networkManager;
+/**
+ History from the request perspective.
+ */
+@property (nonatomic, copy, readonly) NSArray<CR_HttpContent *> *history;
+/**
+ Listener that is called each time that the NetworkManager launch a request.
+ */
+@property (nonatomic, copy) CR_HTTPRequestListener requestListener;
+
+- (instancetype)initWithDeviceInfo:(CR_DeviceInfo*)deviceInfo NS_UNAVAILABLE;
+- (instancetype)initWithNetworkManager:(CR_NetworkManager *)networkManager NS_DESIGNATED_INITIALIZER;
+
+@end
+
+/**
+ Wrapper for the HTTP request and HTTP the response .
+ */
+@interface CR_HttpContent: NSObject
+
+@property (nonatomic, copy, readonly) NSURL *url;
+@property (nonatomic, assign, readonly) CR_HTTPVerb verb;
+@property (nonatomic, copy, readonly) NSDictionary * requestBody;
+@property (nonatomic, copy, readonly) NSData *responseBody;
+@property (nonatomic, strong, readonly) NSError *error;
+@property (nonatomic, assign, readonly) unsigned counter;
+
+- (instancetype)initWithUrl:(NSURL *)url
+                       verb:(CR_HTTPVerb)verb
+                requestBody:(nullable NSDictionary *)requestBody
+               responseBody:(nullable NSData *)responseBody
+                      error:(nullable NSError *)error
+                    counter:(unsigned)counter;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
---
+++ pubsdk/pubsdkTests/Utility/CR_NetworkCaptor.m
@@ -0,0 +1,128 @@
+//
+//  CR_NetworkCaptor.m
+//  pubsdkTests
+//
+//  Created by Romain Lofaso on 11/25/19.
+//  Copyright © 2019 Criteo. All rights reserved.
+//
+
+#import "CR_NetworkCaptor.h"
+
+#import "CR_DeviceInfo.h"
+#import "MockWKWebView.h"
+
+@interface CR_NetworkCaptor ()
+
+/**
+ History from the response perspective.
+ */
+@property (nonatomic, strong) NSMutableArray<CR_HttpContent *> *responseHistory;
+@property (nonatomic, assign) unsigned httpRequestCount;
+
+@end
+
+@implementation CR_NetworkCaptor
+
+- (instancetype)initWithNetworkManager:(CR_NetworkManager *)networkManager
+{
+    MockWKWebView *webView = [[MockWKWebView alloc] init];
+    CR_DeviceInfo *deviceInfo = [[CR_DeviceInfo alloc] initWithWKWebView:webView];
+    self = [super initWithDeviceInfo:deviceInfo];
+    if (self) {
+        _networkManager = networkManager;
+        _responseHistory = [[NSMutableArray alloc] init];
+        _httpRequestCount = 0;
+    }
+    return self;
+}
+
+- (NSArray<CR_HttpContent *> *)history {
+    // Not efficitent but we don't care because this method is called only in tests.
+    return [self.responseHistory sortedArrayUsingComparator:^NSComparisonResult(CR_HttpContent  *_Nonnull obj1, CR_HttpContent *_Nonnull obj2) {
+        if (obj1.counter == obj2.counter) {
+            return NSOrderedSame;
+        } else if (obj1.counter < obj2.counter) {
+            return NSOrderedAscending;
+        } else {
+            return NSOrderedDescending;
+        }
+    }];
+}
+
+- (void)getFromUrl:(NSURL *)url
+    responseHandler:(CR_NMResponse)responseHandler
+{
+    // Synchronized for avoiding multi-thread issue with the httpGetCount.
+    @synchronized (self) {
+        if (self.requestListener != nil) {
+            self.requestListener(url, GET);
+        }
+
+        self.httpRequestCount++;
+        const unsigned count = self.httpRequestCount;
+        [self.networkManager getFromUrl:url responseHandler:^(NSData *data, NSError *error) {
+            CR_HttpContent *content = [[CR_HttpContent alloc] initWithUrl:url
+                                                                     verb:GET
+                                                              requestBody:nil
+                                                             responseBody:data
+                                                                    error:error
+                                                                  counter:count];
+            [self.responseHistory addObject:content];
+            if (responseHandler != nil) {
+                responseHandler(data, error);
+            }
+        }];
+    }
+}
+
+- (void)postToUrl:(NSURL *)url
+         postBody:(NSDictionary *)postBody
+  responseHandler:(CR_NMResponse)responseHandler
+{
+    // Synchronized for avoiding multi-thread issue with the httpPostCount.
+    @synchronized (self) {
+        if (self.requestListener != nil) {
+            self.requestListener(url, POST);
+        }
+        self.httpRequestCount++;
+        const unsigned count = self.httpRequestCount;
+        [self.networkManager postToUrl:url
+                              postBody:postBody
+                       responseHandler:^(NSData *data, NSError *error) {
+            CR_HttpContent *content = [[CR_HttpContent alloc] initWithUrl:url
+                                                                     verb:POST
+                                                              requestBody:postBody
+                                                             responseBody:data
+                                                                    error:error
+                                                                  counter:count];
+            [self.responseHistory addObject:content];
+            if (responseHandler != nil) {
+                responseHandler(data, error);
+            }
+        }];
+    }
+}
+
+@end
+
+@implementation CR_HttpContent
+
+- (instancetype)initWithUrl:(NSURL *)url
+                       verb:(CR_HTTPVerb)verb
+                requestBody:(NSDictionary *)requestBody
+               responseBody:(NSData *)responseBody
+                      error:(NSError *)error
+                    counter:(unsigned)counter
+{
+    if (self = [super init]) {
+        _url = [url copy];
+        _verb = verb;
+        _requestBody = [requestBody copy];
+        _responseBody = [responseBody copy];
+        _error = error;
+        _counter = counter;
+    }
+    return self;
+}
+
+@end
