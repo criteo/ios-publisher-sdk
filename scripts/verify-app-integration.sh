33c58a361081869b8e130106edf1ae370362781f
Romain Lofaso <r.lofaso@criteo.com>
2019-12-02 17:53:27 +0100

    Create a script that verify if the integration of the SDK
    Download the testing app and compile it with the built SDK
    
    Change-Id: I3ab17e0e3983a128e03319122e9ed76362afc337
    JIRA: EE-670
    
    Former-commit-id: 81f49dae3c32364d0cf0d4b1ed7bd1006704a464

---
+++ scripts/verify-app-integration.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#
+# Clone the testing app of fuji, compile it and test it.
+# /!\ Don't forget to compile the SDK before.
+#
+set +x
+set -Eeuo pipefail
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+ROOT_DIR_PATH="${SCRIPT_PATH}/.."
+BUILD_DIR_PATH="${ROOT_DIR_PATH}/build"
+SDK_PATH="${BUILD_DIR_PATH}/output/CriteoPublisherSdk.framework"
+APP_DIR_PATH="${BUILD_DIR_PATH}/testing-app"
+SDK_PATH_IN_APP="${APP_DIR_PATH}/fuji-test-app/AdViewer/CriteoPublisherSdk.framework"
+
+# Check if the SDK has already been compiled
+if [[ ! -d "${SDK_PATH}" ]]; then
+    >&2 echo "[ERROR] The SDK package is missing: ${SDK_PATH}"
+    >&2 echo "[ERROR] Build the SDK first."
+    exit 1
+fi
+
+# Clean up the directory of the testing-app
+rm -rf $APP_DIR_PATH
+mkdir -p $APP_DIR_PATH
+
+# Retrieve and compile the testing app
+cd $APP_DIR_PATH
+git clone https://review.crto.in/pub-sdk/fuji-test-app
+cd fuji-test-app
+cp -R $SDK_PATH $SDK_PATH_IN_APP
+./scripts/compile-test.sh
