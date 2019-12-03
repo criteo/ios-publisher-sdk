72a6f8dabe11a119290cb72ee24b2c7e4aacc2db
Romain Lofaso <r.lofaso@criteo.com>
2019-12-03 16:14:32 +0100

    Add a script dedicated to the presubmit
    Change-Id: I1272626901fbf231fc3fc1b41e677c736abb8625
    JIRA: EE-670
    
    Former-commit-id: 00ac6553005e790059e9f45adb524cbbafc0b565

---
+++ scripts/presubmit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+
+SCRIPT_DIRECTORY="$( cd "$(dirname "$0")" ; pwd -P )"
+BUILD_SCRIPT_PATH="${SCRIPT_DIRECTORY}/build.sh"
+VERIFY_SCRIPT_PATH="${SCRIPT_DIRECTORY}/verify-app-integration.sh"
+
+sh $BUILD_SCRIPT_PATH
+sh $VERIFY_SCRIPT_PATH
