d6f211c2f28c13dc1de710a429aef10bbaa399d3
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-12-04 17:17:05 +0100

    Add tests for Mopub interstitial. Cases: no bid / bid 20 $
    Change-Id: I80fc1ea0d8301f34e558b0e2b035c439a24f1b8a
    JIRA: EE-685
    JIRA: EE-686
    
    Former-commit-id: b1aeb70ec711c0898c45e4f1c8efb50a64f7a8fb

---
+++ pubsdk/Tests/IntegrationTests/CR_MopubInterstitialFunctionalTests.m
@@ -0,0 +1,46 @@
+//
+//  CR_MopubIntegrationFunctionalTests.m
+//  pubsdkTests
+//
+//  Created by Aleksandr Pakhmutov on 03/12/2019.
+//  Copyright © 2019 Criteo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Criteo.h"
+#import "CRInterstitialAdUnit.h"
+#import "CR_IntegrationsTestBase.h"
+#import "MPClasses.h"
+#import "CR_TestAdUnits.h"
+
+static NSString *initialMopubKeywords = @"key1:value1,key2:value2";
+
+@interface CR_MopubInterstitialFunctionalTests : CR_IntegrationsTestBase
+
+@end
+
+@implementation CR_MopubInterstitialFunctionalTests
+
+- (void)test_givenInterstitialWithBadAdUnitId_whenSetBids_thenRequestKeywordsDoNotChange {
+    CRInterstitialAdUnit *interstitial = [CR_TestAdUnits randomInterstitial];
+    [self initCriteoWithAdUnits:@[interstitial]];
+    MPInterstitialAdController *interstitialAdController = [[MPInterstitialAdController alloc] init];
+    interstitialAdController.keywords = initialMopubKeywords;
+
+    [self.criteo setBidsForRequest:interstitialAdController withAdUnit:interstitial];
+
+    XCTAssertEqualObjects(initialMopubKeywords, interstitialAdController.keywords);
+}
+
+- (void)test_givenInterstitialWithGoodAdUnitId_whenSetBids_thenRequestKeywordsUpdated {
+    CRInterstitialAdUnit *interstitial = [CR_TestAdUnits demoInterstitial];
+    [self initCriteoWithAdUnits:@[interstitial]];
+    MPInterstitialAdController *interstitialAdController = [[MPInterstitialAdController alloc] init];
+    interstitialAdController.keywords = initialMopubKeywords;
+
+    [self.criteo setBidsForRequest:interstitialAdController withAdUnit:interstitial];
+
+    [self assertMopubKeywordsUpdated:interstitialAdController.keywords andStillHaveInitialKeywords:initialMopubKeywords];
+}
+
+@end
