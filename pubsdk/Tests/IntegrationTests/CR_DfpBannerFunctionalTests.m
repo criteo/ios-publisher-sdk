a31694f1faaf3c8dbc3aec20154e41adacf864dc
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-12-06 12:25:46 +0100

    Add test for Dfp Banner url encoding
    Change-Id: Id07cbe5df1dbb643ff121967d55f189b68630500
    JIRA: EE-628
    
    Former-commit-id: e3898942df1d32b5bf3d78a18b3b8deeab8bc000

--- pubsdk/Tests/IntegrationTests/CR_DfpBannerFunctionalTests.m
+++ pubsdk/Tests/IntegrationTests/CR_DfpBannerFunctionalTests.m
@@ -13,6 +13,10 @@
 #import "CR_IntegrationsTestBase.h"
 #import "CR_TestAdUnits.h"
 #import "CR_AssertDfp.h"
+#import "Criteo+Internal.h"
+#import "CR_BidManagerBuilder.h"
+#import "CR_AdUnitHelper.h"
+#import "NSString+CR_UrlEncoder.h"
 
 @interface CR_DfpBannerFunctionalTests : CR_IntegrationsTestBase
 
@@ -38,6 +42,29 @@
     [self.criteo setBidsForRequest:bannerDfpRequest withAdUnit:banner];
 
     CR_AssertDfpCustomTargetingContainsCriteoBid(bannerDfpRequest.customTargeting);
+}
+
+- (void)test_givenDfpRequest_whenSetBid_thenDisplayUrlEncodedProperly {
+    CRBannerAdUnit *banner = [CR_TestAdUnits demoBanner320x50];
+    [self initCriteoWithAdUnits:@[banner]];
+    CR_BidManagerBuilder *builder = [self.criteo bidManagerBuilder];
+    CR_CdbBid *bid = [builder.cacheManager getBidForAdUnit:[CR_AdUnitHelper cacheAdUnitForAdUnit:banner]];
+    DFPRequest *bannerDfpRequest = [[DFPRequest alloc] init];
+
+    [self.criteo setBidsForRequest:bannerDfpRequest withAdUnit:banner];
+    NSString *decodedUrl = [self getDecodedDisplayUrlFromDfpRequestCustomTargeting:bannerDfpRequest.customTargeting];
+
+    XCTAssertEqualObjects(bid.displayUrl, decodedUrl);
+}
+
+#pragma mark - Private methods
+
+- (NSString *)getDecodedDisplayUrlFromDfpRequestCustomTargeting:(NSDictionary *)customTargeting {
+    NSString *encodedUrl = customTargeting[@"crt_displayurl"];
+    NSString *unescapedUrl = [[encodedUrl stringByRemovingPercentEncoding] stringByRemovingPercentEncoding];
+    NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:unescapedUrl options:0];
+    NSString *decodedUrl = [[NSString alloc] initWithData:decodedData encoding:NSUTF8StringEncoding];
+    return decodedUrl;
 }
 
 @end
