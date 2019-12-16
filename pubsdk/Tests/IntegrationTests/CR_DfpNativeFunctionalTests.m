05a3888c093170ebacc06591578fb36b1ddb34b4
Aleksandr Pakhmutov <a.pakhmutov@criteo.com>
2019-12-16 16:02:24 +0100

    Check that native payload is encoded before DFP call
    EE-702
    
    Change-Id: I7cb189af1bedc72f29bae8fb47bb47464f8816b0
    
    Former-commit-id: 4a989c46644735c01502e6207a6283107b0f89f7

--- pubsdk/Tests/IntegrationTests/CR_DfpNativeFunctionalTests.m
+++ pubsdk/Tests/IntegrationTests/CR_DfpNativeFunctionalTests.m
@@ -11,6 +11,9 @@
 #import "CR_IntegrationsTestBase.h"
 #import "CR_TestAdUnits.h"
 #import "CR_AssertDfp.h"
+#import "CR_NativeAssets.h"
+#import "CR_TargetingKeys.h"
+#import "NSString+CR_UrlEncoder.h"
 @import GoogleMobileAds;
 
 @interface CR_DfpNativeFunctionalTests : CR_IntegrationsTestBase
@@ -29,7 +32,6 @@
     XCTAssertNil(dfpRequest.customTargeting);
 }
 
-
 - (void)test_givenNativeWithGoodAdUnitId_whenSetBids_thenRequestKeywordsUpdated {
     CRNativeAdUnit *native = [CR_TestAdUnits preprodNative];
     [self initCriteoWithAdUnits:@[native]];
@@ -37,7 +39,55 @@
 
     [self.criteo setBidsForRequest:dfpRequest withAdUnit:native];
 
-    CR_AssertDfpNativeCustomTargetingContainsCriteoBid(dfpRequest.customTargeting);
+    CR_NativeAssets *assets = [self _createNativeAssets];
+    CR_NativeProduct *product = assets.products[0];
+    CR_NativeAdvertiser *advertiser = assets.advertiser;
+    CR_NativePrivacy *privacy = assets.privacy;
+
+    NSDictionary *targeting = dfpRequest.customTargeting;
+
+    XCTAssertNotNil(targeting[CR_TargetingKey_crtCpm]);
+    XCTAssertNil(targeting[CR_TargetingKey_crtDfpDisplayUrl]);
+
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnTitle]], product.title);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnDesc]], product.description);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnImageUrl]], product.image.url);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnPrice]], product.price);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnClickUrl]], product.clickUrl);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnCta]], product.callToAction);
+
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnAdvName]], advertiser.description);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnAdvDomain]], advertiser.domain);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnAdvLogoUrl]], advertiser.logoImage.url);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnAdvUrl]], advertiser.logoClickUrl);
+
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnPrUrl]], privacy.optoutClickUrl);
+    XCTAssertEqualObjects([self _decode:targeting[CR_TargetingKey_crtnPrImageUrl]], privacy.optoutImageUrl);
+
+    XCTAssertEqualObjects([self _decode:targeting[[self _crtnPixUrl:0]]], assets.impressionPixels[0]);
+    XCTAssertEqualObjects([self _decode:targeting[[self _crtnPixUrl:1]]], assets.impressionPixels[1]);
+
+    NSString *pixelCount = [NSString stringWithFormat:@"%@", @(assets.impressionPixels.count)];
+    XCTAssertEqualObjects(targeting[CR_TargetingKey_crtnPixCount], pixelCount);
+}
+
+#pragma mark - Private methods
+
+- (NSString *)_crtnPixUrl:(int)index {
+    return [NSString stringWithFormat:@"%@%d", CR_TargetingKey_crtnPixUrl, index];
+}
+
+- (NSString *)_decode:(NSString *)value {
+    return [NSString decodeDfpCompatibleString:value];
+}
+
+- (CR_NativeAssets *)_createNativeAssets {
+    NSError *e = nil;
+    NSURL *jsonURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"NativeAssetsFromCdb" withExtension:@"json"];
+    NSString *jsonText = [NSString stringWithContentsOfURL:jsonURL encoding:NSUTF8StringEncoding error:&e];
+    NSData *jsonData = [jsonText dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&e];
+    return [[CR_NativeAssets alloc] initWithDict:dictionary];
 }
 
 @end
--- pubsdk/Tests/Utility/CR_AssertDfp.h
+++ pubsdk/Tests/Utility/CR_AssertDfp.h
@@ -6,32 +6,11 @@
 //  Copyright © 2019 Criteo. All rights reserved.
 //
 
-#import "CR_TargetingKeys.h"
-
 #ifndef CR_AssertDfp_h
 #define CR_AssertDfp_h
 
 #define CR_AssertDfpCustomTargetingContainsCriteoBid(customTargeting) \
     XCTAssertEqualObjects(customTargeting[CR_TargetingKey_crtCpm], @"20.00"); \
     XCTAssertNotNil(customTargeting[CR_TargetingKey_crtDfpDisplayUrl]);
-
-#define CR_AssertDfpNativeCustomTargetingContainsCriteoBid(customTargeting) \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtCpm]); \
-    XCTAssertNil(customTargeting[CR_TargetingKey_crtDfpDisplayUrl]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnTitle]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnDesc]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnImageUrl]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnPrice]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnClickUrl]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnCta]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnAdvName]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnAdvDomain]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnAdvLogoUrl]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnAdvUrl]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnPrUrl]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnPrImageUrl]); \
-    XCTAssertNotNil(customTargeting[[CR_TargetingKey_crtnPixUrl stringByAppendingString:@"0"]]); \
-    XCTAssertNotNil(customTargeting[[CR_TargetingKey_crtnPixUrl stringByAppendingString:@"1"]]); \
-    XCTAssertNotNil(customTargeting[CR_TargetingKey_crtnPixCount]);
 
 #endif /* CR_AssertDfp_h */
