56e98ad0c3204979d1d0ef0a4110cc50de4d9184
Vincent Guerci <v.guerci@criteo.com>
2020-03-04 13:47:35 +0100

    [Logs] Header
    To ease referencing from producers & consumers
    
    Change-Id: I1a37dea554a075b01d476a4157463a6b6a5ac31a
    JIRA: EE-928

---
+++ AdViewer/AdViewer/Logs/Logs.h
@@ -0,0 +1,17 @@
+//
+//  Logs.h
+//  AdViewer
+//
+//  Created by Vincent Guerci on 04/03/2020.
+//  Copyright © 2020 Criteo. All rights reserved.
+//
+
+#ifndef Logs_h
+#define Logs_h
+
+#import "LogEntry.h"
+#import "LogManager.h"
+#import "RequestLogEntry.h"
+#import "ResponseLogEntry.h"
+
+#endif /* Logs_h */
