779d6742209cd76ff255f8157d75c9dc5c826bc5
Romain Lofaso <r.lofaso@criteo.com>
2020-03-31 15:25:40 +0200

    Remove warnings in the presubmit console
    Those attachments are not used anymore and flood
    the console of Jenkins build. It will help to clarify what
    happen with the build.
    
    Change-Id: I6085b85d5febb082caead5683d9ce93e2944c5be
    JIRA: EE-993
    
    Former-commit-id: 6f931e7cf7a0c93072fb57d201ea144871c62169

--- pubsdk/Tests/IntegrationTests/CR_IntegrationsTestBase.m
+++ pubsdk/Tests/IntegrationTests/CR_IntegrationsTestBase.m
@@ -16,23 +16,6 @@
     self.criteo = nil;
 }
 
-- (void)tearDown {
-    NSArray<CR_HttpContent *> *requests = self.criteo.testing_networkCaptor.allRequests;
-    for (NSUInteger count = 0; count < requests.count; count++) {
-        CR_HttpContent *r = requests[count];
-        NSString *filename = [[NSString alloc] initWithFormat:@"HTTPRequest_%lu_%@_%@.txt", (unsigned long)count, NSStringFromHTTPVerb(r.verb), r.url];
-        NSData *data = [r.description dataUsingEncoding:NSUTF8StringEncoding];
-
-        XCTAttachment *att = [[XCTAttachment alloc] initWithUniformTypeIdentifier:@"public.utf8-plain-text"
-                                                                             name:filename
-                                                                          payload:data
-                                                                         userInfo:nil];
-        [self addAttachment:att];
-    }
-    
-    [super tearDown];
-}
-
 - (void)initCriteoWithAdUnits:(NSArray<CRAdUnit *> *)adUnits {
     self.criteo = [Criteo testing_criteoWithNetworkCaptor];
     [self.criteo testing_registerAndWaitForHTTPResponseWithAdUnits:adUnits];
