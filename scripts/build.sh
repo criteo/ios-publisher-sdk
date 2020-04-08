70b2f0b1843c7aed1fc7d0a7a5106ad0ee89af9c
Vincent Guerci <v.guerci@criteo.com>
2020-04-08 17:18:29 +0200

    [build] Fix cocoapods / xcpretty export LANG=en_US.UTF-8
    Change-Id: Ibd9be53e5ffd1dc3b739c66fe976d53423537560
    
    Former-commit-id: cd56f88df009d74c4d8cb32e039d11d8fcb39bfd

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,8 @@ set -Eeuo pipefail
 rm -rf build/output
 mkdir -p build/output/sim
 
+export LANG=en_US.UTF-8
+
 CRITEO_WATCH_ARCHS='armv7k arm64_32'
 CRITEO_DEVICE_ARCHS='armv7 armv7s arm64'
 CRITEO_ARCHS="$CRITEO_DEVICE_ARCHS $CRITEO_WATCH_ARCHS"
