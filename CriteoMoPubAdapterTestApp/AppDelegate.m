317920b5c6935aa7d30f2f6216bdcb8def429b15
Vincent Guerci <v.guerci@criteo.com>
2020-06-05 11:52:58 +0200

    [scripts] Source cleanup
    Change-Id: I08623555ab1541feb6e0b7a41932941e58e7d78c

--- CriteoMoPubAdapterTestApp/AppDelegate.m
+++ CriteoMoPubAdapterTestApp/AppDelegate.m
@@ -2,8 +2,7 @@
 //  AppDelegate.m
 //  CriteoMoPubAdapterTestApp
 //
-//  Created by Aleksandr Pakhmutov on 21/01/2020.
-//  Copyright © 2020 Criteo. All rights reserved.
+//  Copyright © 2018-2020 Criteo. All rights reserved.
 //
 
 #import "AppDelegate.h"
--- CriteoMoPubAdapterTestApp/HomePage/HomePageTableViewController.h
+++ CriteoMoPubAdapterTestApp/HomePage/HomePageTableViewController.h
@@ -2,8 +2,7 @@
 //  HomePageTableViewController.h
 //  CriteoMoPubAdapterTestApp
 //
-//  Created by Aleksandr Pakhmutov on 21/01/2020.
-//  Copyright © 2020 Criteo. All rights reserved.
+//  Copyright © 2018-2020 Criteo. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
