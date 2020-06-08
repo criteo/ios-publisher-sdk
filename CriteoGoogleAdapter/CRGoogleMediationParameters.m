b91d463d02443bff8f014a53d5df5075dc092d6d
Vincent Guerci <v.guerci@criteo.com>
2020-06-08 12:57:28 +0200

    [scripts] Source cleanup
    Change-Id: I6549bef57abcf333847dce431ecfb60321cf2125

--- CriteoGoogleAdapter/CRGoogleMediationParameters.m
+++ CriteoGoogleAdapter/CRGoogleMediationParameters.m
@@ -2,7 +2,7 @@
 //  CRGoogleMediationParameters.m
 //  CriteoGoogleAdapter
 //
-//  Copyright © 2019 Criteo. All rights reserved.
+//  Copyright © 2018-2020 Criteo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 //  in compliance with the License. You may obtain a copy of the License at
--- CriteoGoogleAdapterTests/CRGoogleMediationParametersTests.m
+++ CriteoGoogleAdapterTests/CRGoogleMediationParametersTests.m
@@ -2,7 +2,7 @@
 //  CRGoogleMediationParametersTests.m
 //  CriteoGoogleAdapterTests
 //
-//  Copyright © 2019 Criteo. All rights reserved.
+//  Copyright © 2018-2020 Criteo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 //  in compliance with the License. You may obtain a copy of the License at
