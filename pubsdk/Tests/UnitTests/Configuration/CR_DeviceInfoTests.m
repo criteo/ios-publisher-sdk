criteo/ios-publisher-sdk

commit 9a8c47904fb49df25590c02c2c7963820e101075

[DeviceInfo] Fix WebView init deadlock
Quoting apple documentation: https://developer.apple.com/library/archive/documentation/General/Conceptual/ConcurrencyProgrammingGuide/OperationQueues/OperationQueues.html#//apple_ref/doc/uid/TP40008091-CH102-SW28
> Do not call the dispatch_sync function from a task that is executing
> on the same queue that you pass to your function call. Doing so will
> deadlock the queue. If you need to dispatch to the current queue, do
> so asynchronously using the dispatch_async function.

Before fix, we do a dispatch_sync if not on the main thread.
Main Queue targets a specific threadpool, which includes the main
thread, but not only, as a result that could deadlock if we were on
main queue but not main thread. As it is quite complicated to check if
we are on the main queue, let's just lazily instantiate the webview on
main queue and clearly name / hide the constructor that is for testing
only.

Change-Id: Ib05f9102430f2254baa335f70b47c89e5b1bcdae

Former-commit-id: d69e9cc57adafee1b3a374ab4a2aa1ce6520c498

pubsdk/Tests/UnitTests/Configuration/CR_DeviceInfoTests.m
@@ -8,19 +8,13 @@
 #import <XCTest/XCTest.h>
 #import <OCMock.h>
 
-#import "CR_DeviceInfo.h"
+#import "CR_DeviceInfo+Testing.h"
 #import "CR_ThreadManager.h"
 #import "XCTestCase+Criteo.h"
 
 @import WebKit;
 
 @interface CR_DeviceInfoTests : XCTestCase
-
-@end
-
-@interface CR_DeviceInfo (Tests)
-
-@property (strong, nonatomic, readonly) WKWebView *webView;
 
 @end
 
@@ -40,7 +34,7 @@
     OCMStub([wkWebViewMock evaluateJavaScript:@"navigator.userAgent" completionHandler:([OCMArg invokeBlockWithArgs:@"Some Ua", [NSNull null], nil])]);
     CR_ThreadManager *threadManager = [[CR_ThreadManager alloc] init];
     CR_DeviceInfo *deviceInfo = [[CR_DeviceInfo alloc] initWithThreadManager:threadManager
-                                                                     webView:wkWebViewMock];
+                                                                 testWebView:wkWebViewMock];
     [deviceInfo waitForUserAgent:^{
         XCTAssertEqual(@"Some Ua", deviceInfo.userAgent, @"User agent should be set if WKWebView passes it");
         [expectation fulfill];
@@ -58,7 +52,7 @@
     OCMStub([wkWebViewMock evaluateJavaScript:@"navigator.userAgent" completionHandler:([OCMArg invokeBlockWithArgs:@"Not An UA", anError, nil])]);
     CR_ThreadManager *threadManager = [[CR_ThreadManager alloc] init];
     CR_DeviceInfo *deviceInfo = [[CR_DeviceInfo alloc] initWithThreadManager:threadManager
-                                                                     webView:wkWebViewMock];
+                                                                 testWebView:wkWebViewMock];
     [deviceInfo waitForUserAgent:^{
         XCTAssertNil(deviceInfo.userAgent, @"User agent should be nil if we didn't manage to set it. Perhaps we can find a better solution in the future. Also we should log.");
         [expectation fulfill];

pubsdk/Tests/Utility/CR_DeviceInfo+Testing.h
@@ -1,0 +1,17 @@
+//
+//  CR_TestNativeAssets.h
+//  pubsdk
+//
+//  Copyright © 2018-2020 Criteo. All rights reserved.
+//
+
+#import "CR_DeviceInfo.h"
+
+@interface CR_DeviceInfo (Testing)
+
+@property (strong, nonatomic, readonly) WKWebView *webView;
+
+- (instancetype)initWithThreadManager:(CR_ThreadManager *)threadManager
+                          testWebView:(WKWebView *)testWebView;
+
+@end

pubsdk/Tests/Utility/Mocks/CR_DeviceInfoMock.h
@@ -5,7 +5,7 @@
 //  Copyright © 2018-2020 Criteo. All rights reserved.
 //
 
-#import "CR_DeviceInfo.h"
+#import "CR_DeviceInfo+Testing.h"
 
 NS_ASSUME_NONNULL_BEGIN
 

pubsdk/Tests/Utility/Mocks/CR_DeviceInfoMock.m
@@ -17,8 +17,7 @@ NSString * const CR_DeviceInfoMockDefaultUserAgent = @"Mozilla/5.0 (iPhone; CPU 
 - (instancetype)init {
     MockWKWebView *webView = [[MockWKWebView alloc] init];
     CR_ThreadManager *threadManager = [[CR_ThreadManager alloc] init];
-    self = [super initWithThreadManager:threadManager
-                                webView:webView];
+    self = [super initWithThreadManager:threadManager testWebView:webView];
     if (self) {
         _mock_isPhone = YES;
         _mock_isInPortrait = YES;

pubsdk/Tests/Utility/NetworkManager/CR_NetworkCaptor.m
@@ -7,7 +7,7 @@
 
 #import "CR_NetworkCaptor.h"
 
-#import "CR_DeviceInfo.h"
+#import "CR_DeviceInfo+Testing.h"
 #import "MockWKWebView.h"
 #import "CR_ThreadManager.h"
 
@@ -25,7 +25,7 @@
     MockWKWebView *webView = [[MockWKWebView alloc] init];
     CR_ThreadManager *threadManager = [[CR_ThreadManager alloc] init];
     CR_DeviceInfo *deviceInfo = [[CR_DeviceInfo alloc] initWithThreadManager:threadManager
-                                                                     webView:webView];
+                                                                 testWebView:webView];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
     self = [super initWithDeviceInfo:deviceInfo session:session threadManager:threadManager];
     if (self) {

pubsdk/Tests/Utility/NetworkManager/CR_NetworkManagerSimulator.m
@@ -9,7 +9,7 @@
 #import "CR_Config.h"
 #import "CR_NetworkManagerSimulator.h"
 #import "CR_NetworkCaptor.h"
-#import "CR_DeviceInfo.h"
+#import "CR_DeviceInfo+Testing.h"
 #import "CR_ThreadManager.h"
 #import "Criteo+Testing.h"
 #import "MockWKWebView.h"
@@ -41,7 +41,7 @@ NSString *const CR_NetworkSessionEmptyBid = @"{\"slots\":[],\"requestId\":\"c412
     MockWKWebView *webView = [[MockWKWebView alloc] init];
     CR_ThreadManager *threadManager = [[CR_ThreadManager alloc] init];
     CR_DeviceInfo *deviceInfo = [[CR_DeviceInfo alloc] initWithThreadManager:threadManager
-                                                                     webView:webView];
+                                                                 testWebView:webView];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
     if (self = [super initWithDeviceInfo:deviceInfo session:session threadManager:threadManager]) {
         _config = config;

pubsdk/pubsdk.xcodeproj/project.pbxproj
@@ -337,6 +337,7 @@
 
 /* Begin PBXFileReference section */
 		198CD3F7C2A1974769B2CE1C /* CRNativeAdTests.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = CRNativeAdTests.m; sourceTree = "<group>"; };
+		198CD49D6226F38F4FEACE9E /* CR_DeviceInfo+Testing.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = "CR_DeviceInfo+Testing.h"; sourceTree = "<group>"; };
 		198CDC22999EF696C16309C5 /* CASBoundedFileObjectQueue.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = CASBoundedFileObjectQueue.m; sourceTree = "<group>"; };
 		198CDC91DC407C38A0C16DA1 /* CASBoundedFileObjectQueue.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = CASBoundedFileObjectQueue.h; sourceTree = "<group>"; };
 		1CDD7544323F61638E0E8B7F /* libPods-CriteoPublisherSdk.a */ = {isa = PBXFileReference; explicitFileType = archive.ar; includeInIndex = 0; path = "libPods-CriteoPublisherSdk.a"; sourceTree = BUILT_PRODUCTS_DIR; };
@@ -1009,14 +1010,15 @@
 			isa = PBXGroup;
 			children = (
 				7D523C8923E046D400E79C88 /* Categories */,
+				5E065F6AC812E6AF04EF98AB /* Checkers */,
 				7D523C8423DECF7F00E79C88 /* Mocks */,
+				7DB5F8F123AA2B2500EF5602 /* NetworkManager */,
 				7D523C7F23DB5D3C00E79C88 /* Builders */,
 				7DB4E921239A4AB3008DE5E3 /* CR_AssertDfp.h */,
 				7DB4E91B239A468A008DE5E3 /* CR_AssertMopub.h */,
+				198CD49D6226F38F4FEACE9E /* CR_DeviceInfo+Testing.h */,
 				5E065ED9A843A9220D853806 /* CR_TestAdUnits.h */,
 				5E0655500052DF1F93D28212 /* CR_TestAdUnits.m */,
-				5E065F6AC812E6AF04EF98AB /* Checkers */,
-				7DB5F8F123AA2B2500EF5602 /* NetworkManager */,
 				7D51ABEE23AB781600540094 /* CR_Timer.h */,
 				7D51ABEF23AB781600540094 /* CR_Timer.m */,
 				7D58582823E1E6BB0039AC56 /* CR_ThreadManagerWaiter.h */,

pubsdk/pubsdk/Configuration/CR_DeviceInfo.h
@@ -21,9 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (copy, atomic) NSString *userAgent;
 @property (copy, nonatomic, readonly) NSString *deviceId;
 
-- (instancetype)initWithThreadManager:(CR_ThreadManager *)threadManager;
 - (instancetype)initWithThreadManager:(CR_ThreadManager *)threadManager
-                              webView:(WKWebView *)webView
 NS_DESIGNATED_INITIALIZER;
 
 - (void)waitForUserAgent:(void (^ _Nullable)(void))completion;

commit c279a90e3aca7e66554fed704d3b7bd14275c29f

[Tests] Fix DeviceInfoTests.testWebViewReleasedAfterUse
Broken by 9a8c47904fb49df25590c02c2c7963820e101075 [formerly d69e9cc57adafee1b3a374ab4a2aa1ce6520c498]

Change-Id: I0d52377c8490c258d133b5f9ea8b5012c66defd9

Former-commit-id: b9ef71f3994af252df3471ca3663ee0f00c3cad3

pubsdk/Tests/UnitTests/Configuration/CR_DeviceInfoTests.m
@@ -75,10 +75,15 @@
 
 - (void)testWebViewReleasedAfterUse {
     XCTestExpectation *expectation = [self expectationWithDescription:@"WebView has been released"];
-    CR_DeviceInfo *device = [[CR_DeviceInfo alloc] init];
-    XCTAssertNotNil(device.webView, @"WebView has been allocated");
-    [device waitForUserAgent:^{
-        XCTAssertNil(device.webView, @"WebView has been released");
+    WKWebView *wkWebViewMock = OCMStrictClassMock([WKWebView class]);
+    NSError *anError = [NSError errorWithDomain:NSCocoaErrorDomain code:1 userInfo:nil];
+    OCMStub([wkWebViewMock evaluateJavaScript:@"navigator.userAgent"
+                            completionHandler:([OCMArg invokeBlockWithArgs:@"Not An UA", anError, nil])]);
+    CR_ThreadManager *threadManager = [[CR_ThreadManager alloc] init];
+    CR_DeviceInfo *deviceInfo = [[CR_DeviceInfo alloc] initWithThreadManager:threadManager
+                                                                 testWebView:wkWebViewMock];
+    [deviceInfo waitForUserAgent:^{
+        XCTAssertNil(deviceInfo.webView, @"WebView has been released");
         [expectation fulfill];
     }];
     [self cr_waitForExpectations:@[expectation]];