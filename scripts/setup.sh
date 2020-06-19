4f069a3771708d776271f424d6d324a65dc6a346
Vincent Guerci <v.guerci@criteo.com>
2020-06-19 13:16:13 +0200

    [scripts] setup: enable xcode // build
    Change-Id: I667ebefdab5e36b1ce20093730454b45b012542d
    
    Former-commit-id: ee1d7a30992234b58fa856444b05679b1c60883e

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,3 +12,10 @@ xcrun simctl spawn booted log config --mode "level:off"  --subsystem com.apple.C
 # Install Azure CLI with Homebrew for releasing
 # https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-macos?view=azure-cli-latest
 brew update && brew install azure-cli
+
+# Enable parallel builds on Xcode
+num_cpu=$(sysctl -n hw.ncpu)
+defaults write com.apple.dt.xcodebuild PBXNumberOfParallelBuildSubtasks "$num_cpu"
+defaults write com.apple.dt.xcodebuild IDEBuildOperationMaxNumberOfConcurrentCompileTasks "$num_cpu"
+defaults write com.apple.dt.Xcode PBXNumberOfParallelBuildSubtasks "$num_cpu"
+defaults write com.apple.dt.Xcode IDEBuildOperationMaxNumberOfConcurrentCompileTasks "$num_cpu"
