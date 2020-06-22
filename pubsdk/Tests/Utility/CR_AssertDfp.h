5949afdcb80638e84313503b414628a597d62e2e
Vincent Guerci <v.guerci@criteo.com>
2020-06-22 15:11:50 +0200

    [Tests] Fix Dfp custom targeting
    Change-Id: I369fdbcd95577e3088cd9f188a9a87690828ea42
    
    Former-commit-id: 0adaf78ba8d8458a5e1f9b0872d48796619583e6

--- pubsdk/Tests/Utility/CR_AssertDfp.h
+++ pubsdk/Tests/Utility/CR_AssertDfp.h
@@ -9,7 +9,7 @@
 #define CR_AssertDfp_h
 
 #define CR_AssertDfpCustomTargetingContainsCriteoBid(customTargeting) \
-    XCTAssertEqualObjects(customTargeting[CR_TargetingKey_crtCpm], @"20.00"); \
+    XCTAssertEqualObjects(customTargeting[CR_TargetingKey_crtCpm], @"1.12"); \
     XCTAssertNotNil(customTargeting[CR_TargetingKey_crtDfpDisplayUrl]);
 
 #endif /* CR_AssertDfp_h */
