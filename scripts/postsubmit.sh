4a513523ecb5a4be4dc9c4389efbbc9e466b3e7c
Vincent Guerci <v.guerci@criteo.com>
2020-06-24 08:47:59 +0200

    [scripts] Remove deprecated test.sh
    Change-Id: I0de7617e594c2c2964a8433bf84462d4ce8c2dde
    
    Former-commit-id: 86445bc9bd0ce36608fe2d9c41d2f5d8e787b537

--- scripts/postsubmit.sh
+++ scripts/postsubmit.sh
@@ -10,7 +10,6 @@ SCRIPT_DIRECTORY="$(
 # shellcheck source=scripts/base.sh
 source "$SCRIPT_DIRECTORY"/base.sh
 "${SCRIPT_DIRECTORY}"/setup.sh
-#"${SCRIPT_DIRECTORY}"/test.sh "PostsubmitTests"
 
 bundle exec fastlane tests
 
--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -10,6 +10,5 @@ SCRIPT_DIRECTORY="$(
 # shellcheck source=scripts/base.sh
 source "$SCRIPT_DIRECTORY"/base.sh
 "${SCRIPT_DIRECTORY}"/setup.sh
-#"${SCRIPT_DIRECTORY}"/test.sh "PresubmitTests"
 
 bundle exec fastlane tests
