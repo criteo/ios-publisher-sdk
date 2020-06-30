ec77f2cfeec989592c4276b3743e190d1eea39d7
Vincent Guerci <v.guerci@criteo.com>
2020-06-30 16:56:31 +0200

    [scripts] Refactor archive.sh into base.sh
    To be reusable from other repos and clean duplication
    Also remove now useless simulator destination
    
    Change-Id: Ie3288c33a779a35a07b0fbf2e7192292fedf1bc3
    JIRA: EE-1159
    
    Former-commit-id: a2e0d9f13fe71436b4a09f2bd0ef6e240b738511

--- scripts/base.sh
+++ scripts/base.sh
@@ -9,13 +9,6 @@ export CRITEO_DEVICE_ARCHS='armv7 armv7s arm64'
 export CRITEO_ARCHS="$CRITEO_DEVICE_ARCHS $CRITEO_WATCH_ARCHS"
 export CRITEO_SIM_ARCHS='i386 x86_64'
 
-# Configuration for compiling the project for the simulator.
-# We could set a fixed OS version instead of the "latest" to produce the same output from any env
-export XCODEBUILD_DESTINATION_SIMULATOR_OS="latest"
-export XCODEBUILD_DESTINATION_SIMULATOR_DEVICE="iPhone Xs"
-export XCODEBUILD_DESTINATION_SIMULATOR_NAME="Fuji PreSubmit Tests Simulator"
-export XCODEBUILD_DESTINATION_SIMULATOR="platform=iOS Simulator,name=${XCODEBUILD_DESTINATION_SIMULATOR_NAME},OS=${XCODEBUILD_DESTINATION_SIMULATOR_OS}"
-
 # Note: writes to STDERR to prevent breaking xcpretty
 function fuji-printf() { printf "[🏔 fuji] %s" "$@" 1>&2; }
 function fuji-echo() { printf "[🏔 fuji] %s\n" "$@" 1>&2; }
@@ -30,3 +23,73 @@ function fuji-clean() {
 # Stdout with xcpretty while putting full log in a file
 export XCODEBUILD_LOG=$BUILD_PATH/xcodebuild.log
 function fuji-pretty() { tee -a $XCODEBUILD_LOG | xcpretty "$@"; }
+
+function fuji-build-simulator() {
+  fuji-echo "[simulator] build($*)"
+  xcodebuild \
+    -workspace CriteoPublisherSdk.xcworkspace \
+    -scheme CriteoPublisherSdk \
+    -configuration "$CRITEO_CONFIGURATION" \
+    -IDEBuildOperationMaxNumberOfConcurrentCompileTasks="$(sysctl -n hw.ncpu)" \
+    -derivedDataPath build/DerivedData \
+    -sdk iphonesimulator \
+    ARCHS="$CRITEO_SIM_ARCHS" \
+    VALID_ARCHS="$CRITEO_SIM_ARCHS" \
+    ONLY_ACTIVE_ARCH=NO \
+    "$@"
+}
+
+function fuji-build-device() {
+  fuji-echo "[device] build($*)"
+  xcodebuild \
+    -workspace CriteoPublisherSdk.xcworkspace \
+    -scheme CriteoPublisherSdk \
+    -configuration "$CRITEO_CONFIGURATION" \
+    -IDEBuildOperationMaxNumberOfConcurrentCompileTasks="$(sysctl -n hw.ncpu)" \
+    -derivedDataPath build/DerivedData \
+    -sdk iphoneos \
+    ARCHS="$CRITEO_ARCHS" \
+    VALID_ARCHS="$CRITEO_ARCHS" \
+    ONLY_ACTIVE_ARCH=NO \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO \
+    OTHER_CFLAGS="-fembed-bitcode" \
+    "$@"
+}
+
+function fuji-fat-build() {
+  CRITEO_CONFIGURATION=${1:-Debug}
+  fuji-echo "Building $CRITEO_CONFIGURATION..."
+
+  mkdir -p "$BUILD_PATH"/simulator
+  fuji-build-simulator clean build | fuji-pretty
+  cp -R "build/DerivedData/Build/Products/$CRITEO_CONFIGURATION-iphonesimulator/CriteoPublisherSdk.framework" "$BUILD_PATH"/simulator
+
+  mkdir -p "$BUILD_PATH"/device
+  fuji-build-device build | fuji-pretty
+  cp -R "build/DerivedData/Build/Products/$CRITEO_CONFIGURATION-iphoneos/CriteoPublisherSdk.framework" "$BUILD_PATH"/device
+  cp -R "$BUILD_PATH"/device/CriteoPublisherSdk.framework "$BUILD_PATH"
+  rm "$BUILD_PATH"/CriteoPublisherSdk.framework/CriteoPublisherSdk
+
+  lipo -create -output "$BUILD_PATH"/CriteoPublisherSdk.framework/CriteoPublisherSdk "$BUILD_PATH"/simulator/CriteoPublisherSdk.framework/CriteoPublisherSdk "$BUILD_PATH"/device/CriteoPublisherSdk.framework/CriteoPublisherSdk
+  fuji-echo "Fat Binary Contents for $CRITEO_CONFIGURATION Build:"
+  objdump -macho -universal-headers -arch all "$BUILD_PATH"/CriteoPublisherSdk.framework/CriteoPublisherSdk
+}
+
+function fuji-archive() {
+  pushd "$BUILD_PATH"
+  fuji-echo "Archiving..."
+  zip -r "CriteoPublisherSdk.framework.$CRITEO_CONFIGURATION.zip" CriteoPublisherSdk.framework
+  popd
+}
+
+function fuji-remove-headers() {
+  pushd "$BUILD_PATH"/CriteoPublisherSdk.framework/Headers
+  fuji-echo "Removing public headers..."
+  for header in "$@"; do {
+    fuji-echo "Removing $header"
+    sed -i '' "/$header/d" CriteoPublisherSdk.h
+    rm $header
+  }; done
+  popd
+}
