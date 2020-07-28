7cc8f61bf590a451a51670933459a3b908161b11
Vincent Guerci <v.guerci@criteo.com>
2020-07-28 17:19:37 +0200

    [profile-id] prefetch E2E tests

---
+++ CriteoPublisherSdk/Tests/IntegrationTests/CR_ProfileIdFunctionalTests.m
@@ -0,0 +1,110 @@
+//
+//  CR_ProfileIdFunctionalTests.m
+//  CriteoPublisherSdkTests
+//
+//  Copyright © 2018-2020 Criteo. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import <FunctionalObjC/FBLFunctional.h>
+#import <XCTest/XCTest.h>
+
+#import "Criteo+Testing.h"
+#import "Criteo+Internal.h"
+#import "CR_DependencyProvider+Testing.h"
+#import "CR_IntegrationRegistry.h"
+#import "CR_NetworkCaptor.h"
+#import "CR_NetworkWaiter.h"
+#import "CR_NetworkWaiterBuilder.h"
+#import "CR_TestAdUnits.h"
+#import "CR_ThreadManager+Waiter.h"
+#import "NSURL+Testing.h"
+
+@interface CR_ProfileIdFunctionalTests : XCTestCase
+
+@property(strong, nonatomic) Criteo *criteo;
+
+@end
+
+@implementation CR_ProfileIdFunctionalTests
+
+- (void)setUp {
+  [super setUp];
+
+  CR_DependencyProvider *dependencyProvider =
+      CR_DependencyProvider.new.withIsolatedUserDefaults.withWireMockConfiguration
+          .withListenedNetworkManager.withIsolatedFeedbackStorage.withIsolatedNotificationCenter;
+
+  self.criteo = [[Criteo alloc] initWithDependencyProvider:dependencyProvider];
+}
+
+- (void)tearDown {
+  CR_ThreadManager *threadManager = self.criteo.dependencyProvider.threadManager;
+  [threadManager waiter_waitIdle];
+  [super tearDown];
+}
+
+#pragma mark - Prefetch
+
+- (void)testPrefetch_GivenSdkUsedForTheFirstTime_UseFallbackProfileId {
+  CRBannerAdUnit *adUnit = [CR_TestAdUnits preprodBanner320x50];
+  [self prepareCriteoForGettingBidWithAdUnits:@[ adUnit ]];
+
+  NSDictionary *request = [self cdbRequest];
+  XCTAssertEqualObjects(request[@"profileId"], @(CR_IntegrationFallback));
+}
+
+- (void)testPrefetch_GivenUsedSdk_UseLastProfileId {
+  CRBannerAdUnit *adUnit = [CR_TestAdUnits preprodBanner320x50];
+  [self prepareCriteoForGettingBidWithAdUnits:@[ adUnit ]];
+
+  [self getBidResponseWithAdUnit:adUnit];
+
+  NSDictionary *request = [self cdbRequest];
+  XCTAssertEqualObjects(request[@"profileId"], @(CR_IntegrationInHouse));
+}
+
+#pragma mark - Private
+
+- (void)prepareCriteoForGettingBidWithAdUnits:(NSArray *)adUnits {
+  [self.criteo testing_registerWithAdUnits:adUnits];
+  [self.criteo testing_waitForRegisterHTTPResponses];
+}
+
+- (void)getBidResponseWithAdUnit:(CRAdUnit *)adUnit {
+  [self.criteo.testing_networkCaptor clear];
+  [self.criteo getBidResponseForAdUnit:adUnit];
+  [self waitForBid];
+}
+
+- (void)waitForBid {
+  CR_NetworkWaiterBuilder *builder =
+      [[CR_NetworkWaiterBuilder alloc] initWithConfig:self.criteo.config
+                                        networkCaptor:self.criteo.testing_networkCaptor];
+  CR_NetworkWaiter *waiter = builder.withBid.withFinishedRequestsIncluded.build;
+  const BOOL result = [waiter wait];
+  XCTAssert(result, @"Failed waiting for a bid");
+}
+
+- (NSDictionary *)cdbRequest {
+  NSArray<CR_HttpContent *> *requests = self.criteo.testing_networkCaptor.allRequests;
+  NSUInteger index = [requests indexOfObjectPassingTest:^BOOL(CR_HttpContent *_Nonnull obj,
+                                                              NSUInteger idx, BOOL *_Nonnull stop) {
+    return [obj.url testing_isBidUrlWithConfig:self.criteo.config];
+  }];
+  CR_HttpContent *request = (index != NSNotFound) ? requests[index] : nil;
+  return request.requestBody;
+}
+
+@end
