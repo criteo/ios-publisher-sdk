eb6c5862279a00735f2fe8e636f2e041ef484842
Vincent Guerci <v.guerci@criteo.com>
2020-08-27 17:53:29 +0200

    [AdViewer] Change Sdk imports

--- CriteoAdViewer/Sources/HomePage/HomePageTableViewController.h
+++ CriteoAdViewer/Sources/HomePage/HomePageTableViewController.h
@@ -6,7 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
-@import CriteoPublisherSdk;
+#import <CriteoPublisherSdk/CriteoPublisherSdk.h>
 #import "Criteo+Internal.h"
 
 #define GOOGLEBANNERADUNITID_320x50 @"/140800857/Endeavour_320x50"
