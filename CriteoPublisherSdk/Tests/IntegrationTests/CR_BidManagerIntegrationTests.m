811f2c021bd76f252f1b3dfe12072b571e090f52
Vincent Guerci <v.guerci@criteo.com>
2020-09-11 09:21:51 +0200

    [BidManager][IntegrationTests] No bid Slot
    JIRA: EE-759

--- CriteoPublisherSdk/Tests/IntegrationTests/CR_BidManagerIntegrationTests.m
+++ CriteoPublisherSdk/Tests/IntegrationTests/CR_BidManagerIntegrationTests.m
@@ -147,6 +147,14 @@
   [self checkAnotherPrefetchPopulateCache];
 }
 
+- (void)test_givenPrefetchingBid_whenNoBidModeBid_ShouldNotPopulateCache {
+  [self givenMockedCdbResponseBids:@[ self.noBidModeBid ]];
+  [self whenPrefetchingBid];
+  [self shouldNotPopulateCache];
+
+  [self checkAnotherPrefetchPopulateCache];
+}
+
 - (void)test_givenPrefetchingBid_whenSilenceModeBid_ShouldNotPopulateCache {
   CR_CdbBid *silencedBidMock = OCMPartialMock(self.silenceModeBid);
   [self givenMockedCdbResponseBids:@[ silencedBidMock ]];
@@ -266,6 +274,23 @@
                                         nativeAssets:nil
                                         impressionId:nil];
   XCTAssertTrue(bid.isValid);
+  return bid;
+}
+
+- (CR_CdbBid *)noBidModeBid {
+  CR_CdbBid *bid = [[CR_CdbBid alloc] initWithZoneId:@123
+                                         placementId:self.adUnit1.adUnitId
+                                                 cpm:@"0"
+                                            currency:@"EUR"
+                                               width:@320
+                                              height:@50
+                                                 ttl:0
+                                            creative:nil
+                                          displayUrl:@"https://display.url"
+                                          insertTime:nil
+                                        nativeAssets:nil
+                                        impressionId:nil];
+  XCTAssertTrue(bid.isExpired);
   return bid;
 }
 
