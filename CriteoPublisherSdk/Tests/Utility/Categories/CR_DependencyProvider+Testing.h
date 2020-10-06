5160112c093b7c842c6957a1db236b41adb08e7a
Vincent Guerci <v.guerci@criteo.com>
2020-10-06 09:23:17 +0200

    [Tests] Add delay to network simulator
    To simulate live bid time budget exceeding, AFAIK there is no way to
    "manipulate" time with GCD for tests.

--- CriteoPublisherSdk/Tests/Utility/Categories/CR_DependencyProvider+Testing.h
+++ CriteoPublisherSdk/Tests/Utility/Categories/CR_DependencyProvider+Testing.h
@@ -36,6 +36,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)testing_dependencyProvider;
 
+- (CR_DependencyProvider *)withListenedNetworkManagerWithDelay:(NSTimeInterval)delay;
+
 @end
 
 NS_ASSUME_NONNULL_END
