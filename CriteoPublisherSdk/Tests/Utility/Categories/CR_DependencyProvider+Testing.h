e71c41be6d6ddb30231af19ed4387a02cfdab70b
Vincent Guerci <v.guerci@criteo.com>
2020-10-09 14:31:40 +0200

    [Tests] Add short time budget dependency helper
    The live bid time budget is taken in consideration in ThreadManager
    idle state, for the timeOut blocks, even if they aren't doing
    anything, and we cannot cancel them.
    So reducing the budget can make tests faster.

--- CriteoPublisherSdk/Tests/Utility/Categories/CR_DependencyProvider+Testing.h
+++ CriteoPublisherSdk/Tests/Utility/Categories/CR_DependencyProvider+Testing.h
@@ -33,6 +33,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property(weak, nonatomic, readonly) CR_DependencyProvider *withIsolatedNotificationCenter;
 @property(weak, nonatomic, readonly) CR_DependencyProvider *withIsolatedFeedbackStorage;
 @property(weak, nonatomic, readonly) CR_DependencyProvider *withIsolatedIntegrationRegistry;
+@property(weak, nonatomic, readonly) CR_DependencyProvider *withShortLiveBidTimeBudget;
 
 + (instancetype)testing_dependencyProvider;
 
