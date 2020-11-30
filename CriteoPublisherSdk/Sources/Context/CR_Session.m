7a5c4c62363a66f519b8681193813f6126c9c796
Vincent Guerci <v.guerci@criteo.com>
2020-11-30 16:27:02 +0100

    [Contextual] Add session object with duration
    JIRA: EE-1315

---
+++ CriteoPublisherSdk/Sources/Context/CR_Session.m
@@ -0,0 +1,43 @@
+//
+//  CR_Session.m
+//  CriteoPublisherSdk
+//
+//  Copyright © 2018-2020 Criteo. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "CR_Session.h"
+
+@interface CR_Session ()
+@property(nonatomic, copy) NSDate *startDate;
+@end
+
+@implementation CR_Session
+
+- (id)initWithStartDate:(NSDate *)date {
+  if (self = [super init]) {
+    self.startDate = date;
+  }
+  return self;
+}
+
+- (id)init:(NSDate *)date {
+  return [self initWithStartDate:[NSDate date]];
+}
+
+- (NSTimeInterval)duration {
+  return -self.startDate.timeIntervalSinceNow;
+}
+
+@end
