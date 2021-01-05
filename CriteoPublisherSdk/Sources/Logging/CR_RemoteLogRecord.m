e50889e51d2ac66e9d6add6e1700a3337b6b7e54
p.turpin <p.turpin@criteo.com>
2021-01-05 15:35:39 +0100

    Add placeholder interface for storing remote log records
    JIRA: EE-1352

---
+++ CriteoPublisherSdk/Sources/Logging/CR_RemoteLogRecord.m
@@ -0,0 +1,62 @@
+//
+//  CR_RemoteLogRecord.m
+//  CriteoPublisherSdk
+//
+//  Copyright © 2018-2021 Criteo. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "CR_RemoteLogRecord.h"
+
+@implementation CR_RemoteLogRecord
+
+- (instancetype)initWithTag:(NSString *)tag
+                   severity:(CR_LogSeverity)severity
+                    message:(NSString *)message
+              exceptionType:(NSString *_Nullable)exceptionType {
+  self = [super init];
+  if (self) {
+    _tag = tag;
+    _severity = severity;
+    _message = message;
+    _exceptionType = exceptionType;
+  }
+
+  return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+  self = [super init];
+  if (self) {
+    _tag = [coder decodeObjectOfClass:NSString.class forKey:@"_tag"];
+    _severity = (CR_LogSeverity)[coder decodeIntForKey:@"_severity"];
+    _message = [coder decodeObjectOfClass:NSString.class forKey:@"_message"];
+    _exceptionType = [coder decodeObjectOfClass:NSString.class forKey:@"_exceptionType"];
+  }
+
+  return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+  [coder encodeObject:self.tag forKey:@"_tag"];
+  [coder encodeInt:(int)self.severity forKey:@"_severity"];
+  [coder encodeObject:self.message forKey:@"_message"];
+  [coder encodeObject:self.exceptionType forKey:@"_exceptionType"];
+}
+
++ (BOOL)supportsSecureCoding {
+  return YES;
+}
+
+@end
