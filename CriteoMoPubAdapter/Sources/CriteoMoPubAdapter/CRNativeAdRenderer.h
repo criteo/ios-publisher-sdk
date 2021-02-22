3dc539731f2edf1add6bde438c5264f4dc637e60
Vincent Guerci <v.guerci@criteo.com>
2021-02-22 16:54:43 +0100

    [MoPub] Fix
    - Deprecated delegate method
    - Test fix as MPAdView need a size now
    - Adapter import issue
    - Won’t build with iOS 9, so removing pod iOS constraint as it looks
      like no longer required

--- CriteoMoPubAdapter/Sources/CriteoMoPubAdapter/CRNativeAdRenderer.h
+++ CriteoMoPubAdapter/Sources/CriteoMoPubAdapter/CRNativeAdRenderer.h
@@ -19,6 +19,8 @@
 
 #if __has_include(<MoPub/MoPub.h>)
 #import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDK/MoPub.h>)
+#import <MoPubSDK/MoPub.h>
 #elif __has_include(<MoPubSDKFramework/MoPub.h>)
 #import <MoPubSDKFramework/MoPub.h>
 #else
