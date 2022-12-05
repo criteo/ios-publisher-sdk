8e943484fc66b81142a6f846bf0d9830a10a2b04
Valeriu Popa <valeriu.job@gmail.com>
2022-12-05 10:41:42 +0100

    DPP-3562: google mediation adapter

--- CriteoGoogleAdapter/Sources/CriteoGoogleAdapter/CRGoogleMediationParameters.h
+++ CriteoGoogleAdapter/Sources/CriteoGoogleAdapter/CRGoogleMediationParameters.h
@@ -1,21 +1,21 @@
 //
-//  GoogleMediationParameters.h
+//  CRGoogleMediationParameters.h
 //  CriteoGoogleAdapter
 //
-//  Copyright © 2018-2020 Criteo. All rights reserved.
+//  Copyright © 2018-2022 Criteo. All rights reserved.
 //
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-//  in compliance with the License. You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software distributed under the
-//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-//  express or implied. See the License for the specific language governing permissions and
-//  limitations under the License.
-
-#ifndef CRGoogleMediationParameters_h
-#define CRGoogleMediationParameters_h
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 #import <Foundation/Foundation.h>
 
@@ -24,8 +24,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface CRGoogleMediationParameters : NSObject
 
 - (instancetype)init NS_UNAVAILABLE;
-// Create the object with a string such as {"cpId":"B-056946", "adUnitId":
-// "/140800857/Endeavour_320x50"}
 - (id)initWithPublisherId:(NSString *)publisherId adUnitId:(NSString *)adUnitId;
 + (nullable CRGoogleMediationParameters *)parametersFromJSONString:(NSString *)jsonString
                                                              error:(NSError **)outError;
@@ -36,5 +34,3 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 NS_ASSUME_NONNULL_END
-
-#endif /* GoogleMediationParameters_h */
