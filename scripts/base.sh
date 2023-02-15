23a6112f78a2bb112e601d21983711810f53629f
Valeriu Popa <valeriu.job@gmail.com>
2023-02-15 04:53:40 +0200

    🆕 iOS 11 deployment target, removed arm7 (deprecated) from supported archs

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-export CRITEO_ARCHS='armv7 armv7s arm64'
+export CRITEO_ARCHS='arm64'
 export CRITEO_SIM_ARCHS='x86_64 arm64'
 
 # Note: writes to STDERR to prevent breaking xcpretty
